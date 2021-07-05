f0b34d39899862ccf66152a5660e5f434849e479
alex-dsouza777 <alexdsouza00777@gmail.com>
2021-07-05 12:19:42 +0530

    loops

--- JavaScript New/js/index8.js
+++ JavaScript New/js/index8.js
@@ -1,6 +1,40 @@
 // Loops
 
-for(let i=0; i<100; i++)
+// let a = 34;
+// a += 1;
+// a ++;
+
+// console.log(a);
+// for(let i=0; i<100; i++)
+// {
+//     console.log(i);
+// }
+
+// let j = 0;
+// while(j < 100){
+//     console.log(j+1);
+//     j += 1;
+// }
+
+// let k = 0;
+//  do{
+//      console.log(k+1);
+//      k += 1;
+//  } while (k < 10);
+
+// let arr = [2, 4, 5, 2, 7, 8];
+// arr.forEach(function(element){
+//     console.log(element)
+// })
+
+let obj = {
+    name: "Root",
+    age: 22,
+    type: "Programmer",
+    os: "Windows"
+}
+
+for(let key in obj)
 {
-    console.log(i);
+    console.log(`${key} of object is ${obj[key]}`)
 }
